dc138e782a4ef0ded7cca7a95499472276fa29df
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-07-11 15:51:48 +0200

    tricky frontend git

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ function App () {
         <img src={logo} className="App-logo" alt="logo"/>
         <h1 className="App-title">{message}</h1>
         </header>
+        <p>this is a paragraph</p>
         <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
     </p>
